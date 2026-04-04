6200a9e7c423f5fddff346d267072ce983bc0cc1
DangKhoa <dkhoa3568@example.com>
2026-04-04 08:52:27 +0700

    fix api url

--- DoAnChinh-main/frontend/src/contexts/AuthContext.js
+++ DoAnChinh-main/frontend/src/contexts/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect, useCallback, useMemo } from 'react';
 import axios from 'axios';
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://doan-urzg.onrender.com";
 
 const AuthContext = createContext(null);
 
